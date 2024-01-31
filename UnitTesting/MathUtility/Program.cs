b6bfe350e1c67ff85f10658d74e47b8f2a3fed5b
MadMoth125 <madmoth125@gmail.com>
2024-01-31 10:01:11 -0600

    Update Program.cs

--- UnitTesting/MathUtility/Program.cs
+++ UnitTesting/MathUtility/Program.cs
@@ -2,6 +2,8 @@
 {
 	internal class Program
 	{
+		// The main function isn't used for the purpose
+		// of this example since we're only interested in the unit tests.
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello, World!");
